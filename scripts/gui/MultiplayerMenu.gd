94ef758a27841d9300f4b4b5b4c338e04d0325d3
StormLord <stormslordgames@gmail.com>
2025-05-31 21:24:39 +0200

    fixed bug with multiplayer

--- scripts/gui/MultiplayerMenu.gd
+++ scripts/gui/MultiplayerMenu.gd
@@ -16,12 +16,11 @@ func _on_host_pressed() -> void:
 	MultiplayerManager.become_host()
 
 func _on_join_pressed() -> void:
-	if MultiplayerManager.server:
-		Global.game_controller.start_transition()
-		Global.game_controller.change_2d_scene("res://scenes/multiplayer/multiplayer_level.tscn")
-		Global.game_controller.change_gui_scene("")
-		Global.game_controller.end_transition()
-		MultiplayerManager.join_host()
+	Global.game_controller.start_transition()
+	Global.game_controller.change_2d_scene("res://scenes/multiplayer/multiplayer_level.tscn")
+	Global.game_controller.change_gui_scene("")
+	Global.game_controller.end_transition()
+	MultiplayerManager.join_host()
 
 func _on_back_pressed() -> void:
 	Global.game_controller.change_gui_scene("res://scenes/gui/main_ui.tscn")
